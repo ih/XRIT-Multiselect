53ebaed50d11d56aa116a8101915c04b675041a2
ih <irvin.hwang@gmail.com>
2022-12-06 16:28:17 -0500

    OnHover working in the test scene

--- Runtime/XRMultiselectInteractor.cs
+++ Runtime/XRMultiselectInteractor.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class MultiselectPointer : XRBaseInteractor 
+public class XRMultiselectInteractor : XRDirectInteractor
 {
+    [SerializeField] private GameObject pointer;
+    
+    protected void ActivatePointer()
+    {
+       pointer.SetActive(true); 
+    }
 
     protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
