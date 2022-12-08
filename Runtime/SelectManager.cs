ebcc8bc8d04e5a5f53159789838e1b1ed506523f
ih <irvin.hwang@gmail.com>
2022-12-08 16:04:03 -0500

    Add deselect functionality

--- Runtime/SelectManager.cs
+++ Runtime/SelectManager.cs
@@ -6,16 +6,31 @@ using AbstractionMachines;
 
 public class SelectManager : MonoBehaviour
 {
-    private HashSet<GameObject> _selectedObjects;
+    [SerializeField] private Color highlightColor = Color.green;
+    private List<GameObject> _selectedObjects;
 
     private void Start()
     {
-        _selectedObjects = new HashSet<GameObject>();
+        _selectedObjects = new List<GameObject>();
     }
 
     public void Select(GameObject targetObject)
     {
         _selectedObjects.Add(targetObject);
-        GameObjectUtility.Highlight(targetObject, Color.red);
+        GameObjectUtility.Highlight(targetObject, highlightColor);
+    }
+
+    public void Deselect(GameObject targetObject)
+    {
+        _selectedObjects.Remove(targetObject);
+        GameObjectUtility.UnHighlight(targetObject);
+    }
+
+    public void Clear()
+    {
+        for (int i = _selectedObjects.Count - 1; i >= 0; i--)
+        {
+             Deselect(_selectedObjects[i]);           
+        }
     }
 }
--- Runtime/XRMultiselectInteractor.cs
+++ Runtime/XRMultiselectInteractor.cs
@@ -1,16 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using AbstractionMachines;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRMultiselectInteractor : XRDirectInteractor
 {
     [SerializeField] private SelectManager _selectManager;
+    private bool _isSelecting;
     
     protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
         base.OnHoverEntered(args);
         Debug.Log("Entered a selectable!");
-        _selectManager.Select(args.interactableObject.transform.gameObject);
+        if (_isSelecting)
+        {
+            _selectManager.Select(args.interactableObject.transform.gameObject);           
+        }
+    }
+
+    public void OnTriggerPress()
+    {
+        GetComponent<Renderer>().enabled = true; 
+        _isSelecting = true;
+        if (interactablesHovered.Count == 0)
+        {
+            _selectManager.Clear();
+        }
+        else
+        {
+            foreach (IXRHoverInteractable hoveredObject in interactablesHovered)
+            {
+                _selectManager.Select(((XRBaseInteractable) hoveredObject).gameObject);               
+            }
+
+        }
+    }
+
+    public void OnTriggerRelease()
+    {
+        _isSelecting = false;
+        GetComponent<Renderer>().enabled = false;
     }
 }
